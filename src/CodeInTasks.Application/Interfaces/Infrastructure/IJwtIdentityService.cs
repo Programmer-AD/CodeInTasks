2661c49ae2ef5d3436abd03c4c4417181c4a6f8d
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-05-11 00:36:01 +0300

    Add few JwtIdentityService method declarations to interface

--- src/CodeInTasks.Application/Interfaces/Infrastructure/IJwtIdentityService.cs
+++ src/CodeInTasks.Application/Interfaces/Infrastructure/IJwtIdentityService.cs
@@ -2,6 +2,8 @@
 {
     public interface IJwtIdentityService
     {
-        //TODO: add required methods
+        Task<bool> TrySignInAsync(string username, string password, out string token);
+        Task SetRole(Guid userId, string roleName, bool isHave);
+        Task SetBanned(Guid userId, bool isBanned);
     }
 }
