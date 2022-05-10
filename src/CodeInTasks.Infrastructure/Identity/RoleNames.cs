b6c2e8636cf3b13433c1ef02621ed7a515a95acb
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-05-11 00:54:27 +0300

    Add role names file

---
+++ src/CodeInTasks.Infrastructure/Identity/RoleNames.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeInTasks.Infrastructure.Identity
+{
+    public class RoleNames
+    {
+        public const string Creator = "Creator";
+        public const string Manager = "Manager";
+        public const string Admin = "Admin";
+        public const string Builder = "Builder";
+    }
+}
--- src/CodeInTasks.Web/GlobalUsings.cs
+++ src/CodeInTasks.Web/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using CodeInTasks.Application.Interfaces.Services;
 global using CodeInTasks.Domain;
 global using CodeInTasks.Domain.Enums;
 global using CodeInTasks.Domain.Models;
+global using CodeInTasks.Infrastructure.Identity;
