b2871f7f4f02a11b8740e0fbc42d2bf24455decc
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-05-19 02:50:38 +0300

    Implement seeder

---
+++ src/CodeInTasks.Seeding/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeInTasks.Seeding
+{
+    public static class DependencyInjection
+    {
+        //TODO: Make way to invoke seeding in Web or other dedicated project
+        public static IServiceCollection AddSeeding(this IServiceCollection services)
+        {
+            services.AddScoped<Seeder>();
+
+            return services;
+        }
+    }
+}
---
+++ src/CodeInTasks.Seeding/SeedingOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeInTasks.Seeding
+{
+    public class SeedingOptions
+    {
+        public string AdminEmail { get; set; }
+        public string AdminPassword { get; set; }
+    }
+}
