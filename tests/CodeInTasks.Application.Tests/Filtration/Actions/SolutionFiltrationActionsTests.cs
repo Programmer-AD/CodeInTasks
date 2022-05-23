d3e2984a0f6f83ae32dfd7146a8268230aeb692f
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-05-23 23:27:41 +0300

    Add simple tests for FiltrationActions

--- tests/CodeInTasks.Application.Tests/Filtration/Actions/SolutionFiltrationActionsTests.cs
+++ tests/CodeInTasks.Application.Tests/Filtration/Actions/SolutionFiltrationActionsTests.cs
@@ -1,8 +1,90 @@
-﻿namespace CodeInTasks.Application.Tests.Filtration.Actions
+﻿using CodeInTasks.Application.Abstractions.Dtos.Solution;
+using CodeInTasks.Application.Filtration;
+using CodeInTasks.Application.Filtration.Actions;
+
+namespace CodeInTasks.Application.Tests.Filtration.Actions
 {
     [TestFixture]
     public class SolutionFiltrationActionsTests
     {
-        //TODO: SolutionFiltrationActionsTests
+        private SolutionFilterDto filterDto;
+        private FiltrationPipelineResult<Solution> pipelineResult;
+
+        [SetUp]
+        public void SetUp()
+        {
+            filterDto = new();
+            pipelineResult = new();
+        }
+
+        [Test]
+        public void SenderFilter_SetFilterExpression()
+        {
+            //Currently SenderId is required for filter and matches with current user id
+            filterDto.SenderId = Guid.NewGuid();
+
+
+            SolutionFiltrationActions.SenderFilter(filterDto, pipelineResult);
+
+
+            pipelineResult.FilterExpression.Should().NotBeNull();
+        }
+
+        [Test]
+        public void TaskFilter_WhenTaskIdIsNull_DontSetFilterExpression()
+        {
+            filterDto.TaskId = null;
+
+
+            SolutionFiltrationActions.TaskFilter(filterDto, pipelineResult);
+
+
+            pipelineResult.FilterExpression.Should().BeNull();
+        }
+
+        [Test]
+        public void TaskFilter_WhenTaskIdNotNull_SetFilterExpression()
+        {
+            filterDto.TaskId = Guid.NewGuid();
+
+
+            SolutionFiltrationActions.TaskFilter(filterDto, pipelineResult);
+
+
+            pipelineResult.FilterExpression.Should().NotBeNull();
+        }
+
+        [Test]
+        public void ResultsFilter_WhenResultsIsEmpty_DontSetFilterExpression()
+        {
+            filterDto.Results = Array.Empty<TaskSolutionResult>();
+
+
+            SolutionFiltrationActions.ResultsFilter(filterDto, pipelineResult);
+
+
+            pipelineResult.FilterExpression.Should().BeNull();
+        }
+
+        [Test]
+        public void ResultsFilter_WhenResultsIsNotEmpty_SetFilterExpression()
+        {
+            filterDto.Results = new[] { TaskSolutionResult.Completed };
+
+
+            SolutionFiltrationActions.ResultsFilter(filterDto, pipelineResult);
+
+
+            pipelineResult.FilterExpression.Should().NotBeNull();
+        }
+
+        [Test]
+        public void Ordering_SetsOrderFunction()
+        {
+            SolutionFiltrationActions.Ordering(filterDto, pipelineResult);
+
+
+            pipelineResult.OrderFunction.Should().NotBeNull();
+        }
     }
 }
--- tests/CodeInTasks.Application.Tests/Filtration/Actions/TaskFiltrationActionsTests.cs
+++ tests/CodeInTasks.Application.Tests/Filtration/Actions/TaskFiltrationActionsTests.cs
@@ -1,8 +1,101 @@
-﻿namespace CodeInTasks.Application.Tests.Filtration.Actions
+﻿using CodeInTasks.Application.Abstractions.Dtos.Task;
+using CodeInTasks.Application.Filtration;
+using CodeInTasks.Application.Filtration.Actions;
+
+namespace CodeInTasks.Application.Tests.Filtration.Actions
 {
     [TestFixture]
     public class TaskFiltrationActionsTests
     {
-        //TODO: TaskFiltrationActionsTests
+        private TaskFilterDto filterDto;
+        private FiltrationPipelineResult<TaskModel> pipelineResult;
+
+        [SetUp]
+        public void SetUp()
+        {
+            filterDto = new();
+            pipelineResult = new();
+        }
+
+        [Test]
+        public void CreatorFilter_WhenCreatorIdIsNull_DontSetFilterExpression()
+        {
+            filterDto.CreatorId = null;
+
+
+            TaskFiltrationActions.CreatorFilter(filterDto, pipelineResult);
+
+
+            pipelineResult.FilterExpression.Should().BeNull();
+        }
+
+        [Test]
+        public void CreatorFilter_WhenCreatorIdNotNull_SetFilterExpression()
+        {
+            filterDto.CreatorId = Guid.NewGuid();
+
+
+            TaskFiltrationActions.CreatorFilter(filterDto, pipelineResult);
+
+
+            pipelineResult.FilterExpression.Should().NotBeNull();
+        }
+
+        [Test]
+        public void CategoryFilter_WhenCategoriesIsEmpty_DontSetFilterExpression()
+        {
+            filterDto.Categories = Array.Empty<TaskCategory>();
+
+
+            TaskFiltrationActions.CategoryFilter(filterDto, pipelineResult);
+
+
+            pipelineResult.FilterExpression.Should().BeNull();
+        }
+
+        [Test]
+        public void CategoryFilter_WhenCategoriesIsNotEmpty_SetFilterExpression()
+        {
+            filterDto.Categories = new[] { TaskCategory.Implement };
+
+
+            TaskFiltrationActions.CategoryFilter(filterDto, pipelineResult);
+
+
+            pipelineResult.FilterExpression.Should().NotBeNull();
+        }
+
+        [Test]
+        public void RunnerFilter_WhenRunnersIsEmpty_DontSetFilterExpression()
+        {
+            filterDto.Runners = Array.Empty<RunnerType>();
+
+
+            TaskFiltrationActions.RunnerFilter(filterDto, pipelineResult);
+
+
+            pipelineResult.FilterExpression.Should().BeNull();
+        }
+
+        [Test]
+        public void RunnerFilter_WhenCategoriesIsNotEmpty_SetFilterExpression()
+        {
+            filterDto.Runners = new[] { RunnerType.Dotnet_6_0 };
+
+
+            TaskFiltrationActions.RunnerFilter(filterDto, pipelineResult);
+
+
+            pipelineResult.FilterExpression.Should().NotBeNull();
+        }
+
+        [Test]
+        public void Ordering_SetsOrderFunction()
+        {
+            TaskFiltrationActions.Ordering(filterDto, pipelineResult);
+
+
+            pipelineResult.OrderFunction.Should().NotBeNull();
+        }
     }
 }
