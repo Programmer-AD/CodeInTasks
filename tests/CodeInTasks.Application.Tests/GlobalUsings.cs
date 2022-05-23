cce5fb9eeef80f25e2dab9305a55a20fb6924c09
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-05-24 00:20:39 +0300

    Add references and global usings of SharedTestHelpers at all test projects

--- tests/CodeInTasks.Application.Tests/GlobalUsings.cs
+++ tests/CodeInTasks.Application.Tests/GlobalUsings.cs
@@ -1,7 +1,8 @@
 ﻿global using System;
-global using FluentAssertions;
-global using Moq;
-global using NUnit.Framework;
 global using CodeInTasks.Application.Abstractions.Exceptions;
 global using CodeInTasks.Domain.Enums;
 global using CodeInTasks.Domain.Models;
+global using CodeInTasks.SharedTestHelpers;
+global using FluentAssertions;
+global using Moq;
+global using NUnit.Framework;
--- tests/CodeInTasks.Web.Tests/GlobalUsings.cs
+++ tests/CodeInTasks.Web.Tests/GlobalUsings.cs
@@ -1,3 +1,4 @@
-﻿global using FluentAssertions;
+﻿global using CodeInTasks.SharedTestHelpers;
+global using FluentAssertions;
 global using Moq;
 global using NUnit.Framework;
