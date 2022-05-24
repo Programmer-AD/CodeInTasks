4c51caa0104f07a6df1a8e053204b211b7b3e3a7
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-05-25 01:46:35 +0300

    Add tests for ExpressionConvertExtensions

--- tests/CodeInTasks.Infrastructure.Persistance.Tests/ExpressionConvertExtensionsTests.cs
+++ tests/CodeInTasks.Infrastructure.Persistance.Tests/ExpressionConvertExtensionsTests.cs
@@ -1,8 +1,23 @@
-﻿namespace CodeInTasks.Infrastructure.Persistance.Tests
+﻿using System.Linq.Expressions;
+
+namespace CodeInTasks.Infrastructure.Persistance.Tests
 {
     [TestFixture]
     public class ExpressionConvertExtensionsTests
     {
+        [Test]
+        public void PredicateToFunc_MakesWorkingExpression()
+        {
+            var checkNumber = 2;
+            Expression<Predicate<int>> predicateExpression = x => x > 0;
 
+
+            var funcExpression = predicateExpression.ConvertToFunc();
+
+
+            var func = funcExpression.Compile();
+            var funcResult = func(checkNumber);
+            funcResult.Should().BeTrue();
+        }
     }
 }
