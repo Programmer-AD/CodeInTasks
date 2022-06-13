a428c5a3d9c510ecc362048c002b23c83d0dd8aa
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-06-14 01:37:25 +0300

    Add exceptions for docker provider and remove TimeLimitExceedException (now using System.TimeoutException)

---
+++ backend/Builder/src/CodeInTasks.Builder.Runtime.Abstractions/Exceptions/DockerBuildException.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeInTasks.Builder.Runtime.Abstractions.Exceptions
+{
+    public class DockerBuildException : DockerException
+    {
+        private const string actionName = "build";
+
+        public DockerBuildException()
+            : base(actionName)
+        {
+        }
+
+        public DockerBuildException(string outputText)
+            : base(actionName, outputText)
+        {
+        }
+    }
+}
---
+++ backend/Builder/src/CodeInTasks.Builder.Runtime.Abstractions/Exceptions/DockerRunException.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeInTasks.Builder.Runtime.Abstractions.Exceptions
+{
+    public class DockerRunException : DockerException
+    {
+        private const string actionName = "run";
+
+        public DockerRunException()
+            : base(actionName)
+        {
+        }
+
+        public DockerRunException(string outputText)
+            : base(actionName, outputText)
+        {
+        }
+    }
+}
